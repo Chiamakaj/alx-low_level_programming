d5e7299bae3fdd050cfe06c5a58e5f86c4ab2f96
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-11 09:03:52 -0800

    Outputing strings with C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc usingputs.c 
+#include <stdio.h>
+
+/**
+ * main - Entry point, outputs strings
+ *
+ * Return: Always (0) Success
+ */
+int main(void)
+{
+	puts("Programming is like building a mulitlingual puzzle");
+	return (0);
+}
