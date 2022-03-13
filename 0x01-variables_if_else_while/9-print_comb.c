3dc611247492329b6f13560945f03660da181dfd
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-13 07:42:35 -0700

    print digits with comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,17 +10,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 9 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 	{
 		{
 		putchar(i + '0');
 		}
+		if (i != 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
-	for (i = 9 ; i < 10 ; i++)
-	{
-		putchar(i + '0');
-	}
+	putchar('\n');
 	return (0);
 }
