1d0038302690aa94656c2f1c49ae1455919303fa
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-15 10:22:58 -0700

    making the _putchar program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - writing a program to
+ * print a variable.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * main - The C file that
+ * contains _putchar.
+ *
+ * Return: (0)
+ */
+char _putchar;
+
