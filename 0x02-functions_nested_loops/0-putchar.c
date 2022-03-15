94cbfc92d90559fb68310c465004aef4d3633190
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-15 16:45:32 -0700

    correcting coding style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,7 +9,8 @@
 int main(void)
 {
 	int i = 0;
-	char ch[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};	
+	char ch[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
+
 	while (ch[i] != '\0')
 	{
 		_putchar(ch[i]);
