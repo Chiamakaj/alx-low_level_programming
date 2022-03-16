1b75bc544c3ccefe1a3945de0a75813d6304d0cf
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-15 17:23:42 -0700

    making corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,14 +3,13 @@
 /**
  * main - a function that prints the
  * alphabet in lowercase.
- * @void: Nothing to input
+ * @print_alphabet: Nothing to input
  *
  * Return: void.
  */
 void print_alphabet(void)
 {
 	int i;
-	
 	for (i = 'a' ; i <= 'z' ; i++)
 	{
 		_putchar(i);
