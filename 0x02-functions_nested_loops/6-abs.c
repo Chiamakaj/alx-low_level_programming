ac784e4c999c3d0ee43754e246c1f0910acb10aa
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-17 06:47:14 -0700

    corrections

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,6 +3,7 @@
 /**
  * _abs - function that prints
  * absolute value of a number.
+ * @c: the integer ti input.
  *
  * Return: 0
  */
@@ -11,8 +12,8 @@ int _abs(int c)
 	int a;
 
 	if (c >= 0)
-		return(c);
+		return (c);
 	else if (c < 0)
 		a = c * -1;
-	return(a);
+	return (a);
 }
