19cd6fc2402e12cb92ed1415cbc7c554102fa654
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-21 14:06:27 -0700

    adding pointer copy fnction

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/** _strcpy - a function copies the string pointed to by a
+ * variable to another variable.
+ * @dest: variable to copy to
+ * @src: variable to copy from
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
