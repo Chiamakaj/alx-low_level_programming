d970c70740af77ea7287ad08bdfebc36d222601a
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-22 02:51:46 -0700

    correction

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -16,9 +16,9 @@ char *_strcpy(char *dest, char *src)
 	{
 		dest[i] = src[i];
 	}
-	if (dest[i] == src['\0'])
-	{
-		dest[i] = ' ';
-	}
+		if (dest[i] == src['\0'])
+		{
+			dest[i] = ' ';
+		}
 	return (dest);
 }
