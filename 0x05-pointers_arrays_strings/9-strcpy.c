19aabc4d468fe74ccad71a929711d77a81217be5
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-22 03:55:18 -0700

    corrections

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,16 +12,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i, j;
 
-	for (i = 0 ; dest[i] != '\0' ; i++)
+	for (i = 0 ; src[i] != '\0' ; i++)
 	{
-		for (j = 0 ; src[j] != '\0' ; j++)
-		{
-			dest[i] = src[j];
-			if (src[j] == '\0')
-			{
-				dest[i] = '\0';
-			}
-		}
+			dest[i] = src[i];
 	}
 	return (dest);
 }
