ce1877a6aa7ac984dfb8f5b6514c0c79e197c9f1
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-24 02:38:40 -0700

    capitalization function

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * cap_string - a function that capitalizes all the first
+ * letters of the words in a string.
+ * @c: the string to capitalize
+ *
+ * Return: the capitalized string
+ */
+char *cap_string(char *c)
+{
+	int i;
+
+	for (i = 0 ; c[i] != '\0' ; i++)
+	{
+		if (i == 0)
+		{
+			i++;
+			if (c[i - 1] >= 'a' && c[i - 1] <= 'z')
+			{
+				c[i - 1] = c[i - 1] - 32;
+				continue;
+			}
+		}
+		if (c[i] == '.' && (c[i - 1] >= 'a' && c[i - 1] <= 'z'))
+		{
+			i++;
+			if (c[i] >= 'a' && c[i] <= 'z')
+			{
+				c[i] = c[i] - 32;
+				continue;
+			}
+		}
+		if (c[i] == '\n' || c[i] == '\t' || c[i] == ' ')
+		{
+			i++;
+			if (c[i] >= 'a' && c[i] <= 'z')
+			{
+				c[i] = c[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if(c[i] >= 'A' && c[i] <= 'Z')
+			{
+				c[i] = c[i] + 32;
+			}
+		}
+	}
+	return (c);
+}
