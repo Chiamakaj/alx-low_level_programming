d6a6253d3410021d41e8654f2cc4c0362ff2d397
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-23 17:16:15 -0700

    adding function to convert letter case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - a function that changes all lowercase
+ * letters of a string to uppercase.
+ * @c: the string to change
+ *
+ * Return: capitalizes strings.
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0 ; c[i] != '\0' ; i++)
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+		{
+			c[i] = c[i] - 32;
+		}
+	}
+	return (c);
+}
