ff20334bb064a14e79204ea5af49177b1a81b2d1
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-26 13:55:22 -0700

    betty doc style correction

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers,
+ * print_most_numbers - a function that prints the numbers,
  * from 0 to 9, without printing 2 and 4.
  *
  * Return: void
