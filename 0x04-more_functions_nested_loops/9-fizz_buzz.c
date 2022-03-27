f61799e328f4e408f1ecec9e5ee52f04881ffac9
Chiamakaj <juliet.ohaji@gmail.com>
2022-03-27 16:23:05 -0700

    code correcion

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,17 +18,21 @@ int main(void)
 		{
 			if ((i % 5) == 0)
 			{
-				printf("FizzBuzz ");
+				printf(" FizzBuzz");
 			}
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
