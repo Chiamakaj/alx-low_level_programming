1dd9b948b88188c64f22d70a009b23869b0e1e72
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-09 07:42:09 -0700

    betty code style correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,67 +14,52 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len, len1, len2;
 	char *concatstr;
 
-	if (s1 == NULL)
+	if ((s1 == NULL) || (s2 == NULL))
 	{
-		s1 = "";
+		s1 = s2 = "";
 	}
 
-	if (s2 == NULL)
+	for (len1 = 0 ; s1[len1] != '\0' ;)
 	{
-		s2 = "";
+		len1++;
 	}
 
-	if ((s1 == NULL) && (s2 == NULL))
+	for (len2 = 0 ; s2[len2] != '\0' ;)
 	{
-		s1 = "";
-		s2 = "";
+		len2++;
 	}
 
-	if ((s1 != NULL) || (s2 != NULL))
+	len = len1 + len2;
+		
+	len += 1;
+	
+	concatstr = malloc(sizeof(char) * len);
+		
+	if (concatstr == NULL)
 	{
-		for (len1 = 0 ; s1[len1] != '\0' ;)
-		{
-			len1++;
-		}
-
-		for (len2 = 0 ; s2[len2] != '\0' ;)
-		{
-			len2++;
-		}
-
-		len = len1 + len2;
-		
-		len += 1;
-		
-		concatstr = malloc(sizeof(char) * len);
-		
-		if (concatstr == NULL)
-		{
-			return (NULL);
-		}
-		
-		j = 0;
-		i = 0;
-
-		while (s1[i] != '\0')
-		{
-			concatstr[j] = s1[i];
-			i++;
-			j++;
-		}
-
-		i = 0;
-		
-		while (s2[i] != '\0')
-		{
-			concatstr[j] = s2[i];
-			i++;
-			j++;
-		}
-
-		concatstr[j] = '\0';
-		
-		return (concatstr);
+		return (NULL);
 	}
-	return (0);
+	
+	j = 0;
+	i = 0;
+
+	while (s1[i] != '\0')
+	{
+		concatstr[j] = s1[i];
+		i++;
+		j++;
+	}
+
+	i = 0;
+		
+	while (s2[i] != '\0')
+	{
+		concatstr[j] = s2[i];
+		i++;
+		j++;
+	}
+
+	concatstr[j] = '\0';
+		
+	return (concatstr);
 }
