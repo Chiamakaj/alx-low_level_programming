288a467773eac85f2688d4cf19c74a7f3eb7bd6b
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-09 07:20:25 -0700

    code correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -29,11 +29,13 @@ char *str_concat(char *s1, char *s2)
 	}
 	if ((s1 != NULL) || (s2 != NULL))
 	{
-		for (len1 = 0 ; s1[len1] != '\0' ; len1++)
+		for (len1 = 0 ; s1[len1] != '\0' ;)
 		{
+			len1++;
 		}
-		for (len2 = 0 ; s2[len2] != '\0' ; len2++)
+		for (len2 = 0 ; s2[len2] != '\0' ;)
 		{
+			len2++;
 		}
 		len = len1 + len2;
 		len += 1;
