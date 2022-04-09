998944e67cfe53ad08f60df5cb72eb626a709ac6
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-09 10:00:45 -0700

    betty code style correction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,8 +16,6 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL || s2 == NULL)
 		s1 = s2 = "";
-	if (s1 == NULL && s2 == NULL)
-		s1 = s2 = "";
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 	{
 	}
