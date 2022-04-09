67162678b7e9584ceff2ba8672d4ce238b9b59d5
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-09 06:50:51 -0700

    adding more functions with malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * str_concat - a function that concatenates two strings.
+ * @s1: input string
+ * @s2: input string
+ *
+ * Return: NULL
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j, len, len1, len2;
+	char *concatstr;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return(NULL);
+	}
+	if (s1 != NULL || s2 != NULL)
+	{
+		for (len1 = 0 ; s1[len1] != '\0' ; len1++)
+		{
+		}
+		for (len2 = 0 ; s2[len2] != '\0' ; len2++)
+		{
+		}
+		len = len1 + len2;
+		len += 1;
+		concatstr = malloc(sizeof(char) * len);
+		j = 0;
+		i = 0;
+		while (s1[i] != '\0')
+		{
+			concatstr[j] = s1[i];
+			i++;
+			j++;
+		}
+		i = 0;
+		while (s2[i] != '\0')
+		{
+			concatstr[j] = s2[i];
+			i++;
+			j++;
+		}
+		concatstr[j] = '\0';
+		return (concatstr);
+	}
+	return (0);
+}
