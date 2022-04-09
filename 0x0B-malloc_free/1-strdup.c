dd6de73a7967ad6a0be87d2802b7972275df09f3
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-09 03:51:10 -0700

    code correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,24 +12,21 @@
 char *_strdup(char *str)
 {
 	int i, len;
-	char *array;
+	char *newstring;
 
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 	for (len = 0 ; str[len] != '\0' ; len++)
-	{
-	}
-	len += 1;
-	array = malloc((sizeof(char) * len));
+	newstring = malloc((sizeof(char) * len) + 1);
 	for (i = 0 ; i <= len ; i++)
 	{
-		array[i] = str[i];
+		newstring[i] = str[i];
 	}
-	if (array < str)
+	if (sizeof(newstring) < sizeof(str))
 	{
 		return (NULL);
 	}
-	return (array);
+	return (newstring);
 }
