28f838e6a998667db42d402053c32383162e89b2
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-09 09:07:03 -0700

    adding more malloc functions

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - a function that returns a pointer to a
+ * 2 dimensional array of integers.
+ * @width: number of array rows
+ * @height: number of array colums
+ *
+ * Return: NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **matrix;
+
+	if ((width <= 0) || (height <=0))
+	{
+		return (NULL);
+	}
+
+	matrix = malloc(sizeof(int) * height);
+
+	if (matrix == NULL)
+	{
+		free(matrix);
+		return (NULL);
+	}
+
+	for (i = 0 ; i < height ; i++)
+	{
+		matrix[i] = malloc(sizeof(int) * width);
+		if (matrix == NULL)
+		{
+			free(matrix);
+			return (NULL);
+		}
+
+		for (j = 0 ; j < width ; j++)
+		{
+			matrix[i][j] = 0;
+		}
+	}
+	return (matrix);
+}
