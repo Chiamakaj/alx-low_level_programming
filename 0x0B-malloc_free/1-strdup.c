71f00bd62d4108658ad4e3d7e27c97969e27eae9
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-09 10:08:52 -0700

    betty code style correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -18,24 +18,18 @@ char *_strdup(char *str)
 	{
 		return (NULL);
 	}
-
 	for (len = 0 ; str[len] != '\0' ; len++)
 	{
 	}
-
 	len += 1;
-
 	newstring = malloc(sizeof(char) * len);
-
 	if (newstring == NULL)
 	{
 		return (NULL);
 	}
-
 	for (i = 0 ; i <= len ; i++)
 	{
 		newstring[i] = str[i];
 	}
-
 	return (newstring);
 }
