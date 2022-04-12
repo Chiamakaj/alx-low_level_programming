fb1a9a0f48936651641dc2ae69349f6dea8b378f
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-12 16:20:01 -0700

    Adding my first struct type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - defining a new type
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: a new type called struct dog with
+ * 3 members.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
