c5320861dea9526c951e53bca314d3eb41e4c75d
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-13 16:56:46 -0700

    betty doc style correction

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,6 +5,7 @@
 /**
  * array_iterator - a function that executes a function
  * given as a parameter on each element of an array.
+ * @array: array
  * @size: the size of the array
  * @action: pointer to the function you need to use
  * Return: void
