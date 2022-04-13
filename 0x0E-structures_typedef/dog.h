78d17506ebd1cc61ea6235fd4baf7953541cb015
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-12 17:28:42 -0700

    betty code style correction

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-void init_dog(struct dog *d, char *name, float age, char *owner);
 
-#endif
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif /* DOG_H*/
