efd7334ee580177fdd3d9bbfac585ea2e69fe290
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-18 07:40:00 -0700

    code correction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -32,7 +32,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			printf("%s", str);
 			if (i < (n-1))
 			{
-				printf("%c ", *separator);
+				printf("%c", separator);
 			}
 		}
 	}
