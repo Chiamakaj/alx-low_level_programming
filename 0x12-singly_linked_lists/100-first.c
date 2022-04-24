e8e4da21aa8fdbfb29b11bd0faad88182991034f
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-24 12:53:14 -0700

    adding more functions

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_str - af unctions that prints a string
+ * before the main function is executed.
+ *
+ * Return: void
+ */
+void print_str(char *ch)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
