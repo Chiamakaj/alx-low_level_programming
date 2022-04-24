368e1b76bb3544a97ea89a22ac5e6f9a4c30ca22
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-24 13:09:18 -0700

    code correction

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,7 +6,10 @@
  *
  * Return: void
  */
-void print_str()
+
+void print_str(void) __attribute__ ((constructor));
+
+void print_str(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
