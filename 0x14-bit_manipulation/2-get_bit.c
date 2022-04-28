71417d4bbb24e7c9d645f7acf65be3dcc6635a0b
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-28 16:35:09 -0700

    Betty coding style correction

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,7 +13,8 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int bits;
 
-	bits = (n >> index) & 1;
+	bits = ((n >> index) & 1);
+	
 	if ((bits == 1) || (bits == 0))
 		return (bits);
 	else
