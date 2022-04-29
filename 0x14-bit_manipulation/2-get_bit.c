e7e7e65b93124058288285cee145cb1864fda8e5
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-29 11:40:02 -0700

    code correction

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,9 +13,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int bits;
 
+	if (index >= sizeof(unsigned int) * sizeof(long))
+		return (-1);
 	bits = ((n >> index) & 1);
 	if ((bits == 1) || (bits == 0))
 		return (bits);
-	else
-		return (-1);
+	return (0);
 }
