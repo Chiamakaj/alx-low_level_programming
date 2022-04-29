9b7959a05df38b352c4bbb5fb887800be671c60b
Chiamakaj <juliet.ohaji@gmail.com>
2022-04-29 08:43:00 -0700

    return value correction

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -12,8 +12,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bits;
 
-	if (n == NULL)
-		return (-1);
 	bits = 1 << index;
 	if ((bits | *n) == *n)
 		*n = *n ^ bits;
