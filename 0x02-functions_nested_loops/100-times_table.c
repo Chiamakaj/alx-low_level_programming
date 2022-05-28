f1b3b5480ba0a40083b78695abec823b33c8319d
Chiamakaj <juliet.ohaji@gmail.com>
2022-05-28 10:36:55 -0700

    correcting header file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,11 +13,11 @@ void print(long n)
 		_putchar('-');
 		n = -n;
 	}
-	if (n/10)
+	if (n / 10)
 	{
-		print(n/10);
+		print(n / 10);
 	}
-	_putchar(n%10 + '0');
+	_putchar(n % 10 + '0');
 }
 
 /**
