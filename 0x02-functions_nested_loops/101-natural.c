0aa5617dee0dae867e6612b2ce16d9611746c5bb
Chiamakaj <juliet.ohaji@gmail.com>
2022-05-29 15:10:57 -0700

    code correction

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,16 +7,15 @@
  */
 int main(void)
 {
-	int i;
+	int i, sum = 0;
 
 	for (i = 1 ; i < 1024 ; i++)
 	{
-		if (((i % 3) == 0) || ((i % 5) == 0))
+		if ((i % 3) == 0 || (i % 5) == 0)
 		{
-			i *= i;
+			sum += i;
 		}
 	}
-	printf("%d", i);
-	printf("\n");
+	printf("%d", sum);
 	return (0);
 }
